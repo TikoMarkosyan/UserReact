50ed229ce2aa34b1544233d676836c682af01044
Tiko Markosyan <tikomarkosyan61@gmail.com>
2019-12-12 01:23:16 +0400

    nes

---
+++ src/componets/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default function Header(props) {
+  return <h1 className="title">{props.title}</h1>;
+}
+Header.propTypes = {
+   optionalObject: PropTypes.object
+};
+Header.defaultProps = {
+  title: "somthing went wrong",
+}
